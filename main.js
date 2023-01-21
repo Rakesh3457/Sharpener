1c9276679d0b7c56c3383b22cca7666986e296e6
Rakesh3457 <rgolu252@gmail.com>
2023-01-21 23:14:45 +0530

    commit 1

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("first code for git");
+var headerTitle = document.getElementById('header-title');
+headerTitle.innerText = "Lets Rock";
+
+headerTitle.style.borderBottom = 'solid 5px yellow';
+
+
+const additem=document.getElementsByClassName('title')
+additem[0].style.color = 'green';
