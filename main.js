ce9f8a1d61dda9f91f7065f8f96590d3ed0cf138
Rakesh3457 <rgolu252@gmail.com>
2023-01-22 21:34:07 +0530

    elemets by tag name

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ items[2].style.background ='green';
 for (let i = 0; i < items.length; i++) {
     items[i].style.fontWeight='900';
   }
+
+
+
+  // query Selector
+
+  var li=document.getElementsByTagName('li');
+  for (let i = 0; i < li.length; i++) {
+    li[i].style.background='grey';
+    li[i].style.fontWeight='900';
+  }
