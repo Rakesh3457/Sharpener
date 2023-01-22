913c0688763f863c57ca69939b4edd4ef4547507
Rakesh3457 <rgolu252@gmail.com>
2023-01-22 22:20:03 +0530

    Query Selector

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-var headerTitle = document.getElementById('header-title');
-headerTitle.innerText = "Lets Rock";
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.innerText = "Lets Rock";
 
-headerTitle.style.borderBottom = 'solid 5px yellow';
-
-
-var items=document.getElementsByClassName('list-group-item');
-items[2].style.background ='green';
-for (let i = 0; i < items.length; i++) {
-    items[i].style.fontWeight='900';
-  }
+// headerTitle.style.borderBottom = 'solid 5px yellow';
 
 
+// var items=document.getElementsByClassName('list-group-item');
+// items[2].style.background ='green';
+// for (let i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight='900';
+//   }
 
-  // query Selector
 
-  var li=document.getElementsByTagName('li');
-  for (let i = 0; i < li.length; i++) {
-    li[i].style.background='grey';
-    li[i].style.fontWeight='900';
-  }
+
+//   // tag Selector
+
+//   var li=document.getElementsByTagName('li');
+//   for (let i = 0; i < li.length; i++) {
+//     li[i].style.background='grey';
+//     li[i].style.fontWeight='900';
+//   } 
+
+// Query Selector
+
+var li=document.querySelector('.list-group-item:nth-child(2)');
+li.style.background= 'green';
+
+
+var thirdli=document.querySelector('.list-group-item:nth-child(3)');
+thirdli.style.display='none';
